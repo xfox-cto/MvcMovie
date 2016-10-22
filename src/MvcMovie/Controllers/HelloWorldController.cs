4088ac99a1c3a6dea23b0f1cb3872155853977c7
xfox <xfox.cto@live.com>
2016-10-22 20:18:51 +0800

    初步

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /helloworld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /helloworld/welcome
+        public string Welcome()
+        {
+            return "This is Welcom action method...";
+        }
+    }
+}
